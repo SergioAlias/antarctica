ba9aee61eea297e53017486947e42b1f0bcb855d
SergioAlias <sergio08071998@gmail.com>
2025-07-10 11:48:57 +0200

    First script

---
+++ README.md
@@ -0,0 +1,1 @@
+Scripts for the metataxonomic analysis (16S/ITS) of Antarctica permafrost
---
+++ taxonomy_tree.R
@@ -0,0 +1,10 @@
+# ╔═══════════════════════════════════════════════════════════════════╗
+# ║                      taxonomy_tree.R                              ║
+# ╠═══════════════════════════════════════════════════════════════════╣
+# ║ Project        : antarctica                                       ║
+# ║ Author         : Sergio Alías-Segura                              ║
+# ║ Created        : 2025-07-10                                       ║
+# ║ Last Modified  : 2025-07-10                                       ║
+# ║ GitHub Repo    : https://github.com/SergioAlias/antarctica        ║
+# ║ Contact        : salias[at]ucm[dot]es                             ║
+# ╚═══════════════════════════════════════════════════════════════════╝
